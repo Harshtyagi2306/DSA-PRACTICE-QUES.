0a9d129951f9ef8b6b14ce68c555b4d0ae1be184
Harshtyagi-co <84906229+Harshtyagi-co@users.noreply.github.com>
2023-03-19 00:33:37 +0530

    Add files via upload

---
+++ arrayUse.java
@@ -0,0 +1,13 @@
+package Array;
+public class arrayUse {
+    public static void main(String[] args) {
+        
+    
+    int[] arr = new int[10];
+    arr[3] = 15;
+
+    System.out.println(arr[3]);
+
+    //by default all array index has value=0
+    }
+}
---
+++ arrayoperation.java
@@ -0,0 +1,14 @@
+package Array;
+public class arrayoperation {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        char chArray[] = new char[11];
+        chArray[0] = 'a';
+        double dArray[] = new double[15];
+        dArray[0] = 3;
+
+        System.out.println(chArray[0]);
+        System.out.println(dArray[3]);
+    }
+    
+}
